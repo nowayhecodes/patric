a602d4ae4516608ee52ba229e77a1473a2ee9919
Gustavo Cavalcante <nowayhecodes@gmail.com>
2020-08-12 03:14:16 -0300

    Add cli.spec.ts

---
+++ src/__test__/cli.spec.ts
@@ -0,0 +1,10 @@
+import { cpus } from 'os';
+import { exec, fork, spawn } from 'child_process';
+
+describe('CLI test suite', () => {
+  it('Invokes the start command', () => {
+    // exec('npm run cli -- start', () => {
+
+    // });
+  });
+});
--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,6 +1,8 @@
-import yargs, { Arguments } from 'yargs';
+import yargs, { Arguments, check } from 'yargs';
+import { cpus } from 'os';
+import { fork, isMaster, isWorker, worker } from 'cluster';
 
-const argv = yargs
+const argv: Arguments = yargs
   .command('start', 'Start the application', {
     start: {
       description: 'starts the application',
@@ -13,11 +15,23 @@ const argv = yargs
     description: 'Specify the mode to start',
     type: 'string',
     choices: ['dev', 'stage', 'prod'],
+    default: 'dev',
+  })
+  .option('cluster', {
+    alias: 'c',
+    description:
+      'Indicates wether you want to run the application in cluster mode',
+    type: 'boolean',
+    default: false,
+  })
+  .option('ui', {
+    description: 'Pass true if you want to start with the web interface',
+    type: 'boolean',
+    default: false,
   })
   .help()
   .alias('help', 'h').argv;
 
 if (argv.mode) {
-    console.log('Running in development mode');
-    
+  console.log('Running in development mode');
 }
