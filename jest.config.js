fe76c1300dc473a7111e410979fb3875375f35b2
Gustavo Cavalcante <nowayhecodes@gmail.com>
2020-08-12 19:41:26 -0300

    mock execSync() call

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '\\.*.(ts|tsx?)$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts?|tsx?)$',
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
--- src/__test__/cli.spec.ts
+++ src/__test__/cli.spec.ts
@@ -1,10 +1,24 @@
-import { cpus } from 'os';
-import { exec, fork, spawn } from 'child_process';
+import * as child_process from 'child_process';
+import { mocked } from 'ts-jest/utils';
 
 describe('CLI test suite', () => {
-  it('Invokes the start command', () => {
-    // exec('npm run cli -- start', () => {
+  describe('With mocks', () => {
+    it('Successfully invokes the start command', () => {
+      jest.mock('child_process');
+      const mockedChildProcess = mocked(child_process, true);
+      expect(
+        mockedChildProcess.execSync('npm run cli -- start')
+      ).toHaveBeenCalled();
 
-    // });
+      jest.clearAllMocks();
+    });
+  });
+
+  describe('Without mocks', () => {
+    it('Includes "start" as command', () => {
+      expect(
+        child_process.execSync('npm run cli -- start').includes('start')
+      ).toBeTruthy();
+    });
   });
 });
